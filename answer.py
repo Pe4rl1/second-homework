c7302009b0f6507fb05eecd961ca9775d4a24eb7
Pearl <alexander.multan>
2022-02-22 01:02:11 +0300

    changed answer.py

---
+++ answer.py
@@ -0,0 +1,9 @@
+a = input('')
+b = input('')
+c = input('')
+task1 = (a**2 + b**2) / (3*b - 4)
+task2 = 4*c**5 / 6
+result1 = task1 / task2
+result2 = float(result1)
+result3 = int(result2)
+result4 = result2 - result3
