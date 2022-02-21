5407b239a181bf4c6cbb7e87779900045b2ad8c5
Pearl <alexander.multan>
2022-02-22 01:06:36 +0300

    lalala

--- answer.py
+++ answer.py
@@ -4,6 +4,6 @@ c = input('')
 task1 = (a**2 + b**2) / (3*b - 4)
 task2 = 4*c**5 / 6
 result1 = task1 / task2
-result2 = float(result1)
-result3 = int(result2)
-result4 = result2 - result3
+result3 = int(result1)
+result4 = result1 - result3
+print(result4)
