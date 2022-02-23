77972ed7b6c9ec4959b6dcf379c4138b999ed822
Pearl <alexander.multan>
2022-02-23 19:05:23 +0300

    .

--- answer.py
+++ answer.py
@@ -1,6 +1,6 @@
-a = input('')
-b = input('')
-c = input('')
+a = int(input(''))
+b = int(input(''))
+c = int(input(''))
 task1 = a**2 + b**2
 task2 = 3*b - 4
 task3 = task1 / task2
@@ -8,4 +8,7 @@ task4 = 4*c**5 / 6
 result1 = task3 / task4
 result3 = int(result1)
 result4 = result1 - result3
+print(result1)
+print(result3)
 print(result4)
+
